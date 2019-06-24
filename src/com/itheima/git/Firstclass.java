b56363ec928b5da09810143f48ddaec3b982ab45
yanggaochun <2672381524@qq.com>
2019-06-24 11:55:48 +0800

    修改了Firstclass内容

--- src/com/itheima/git/Firstclass.java
+++ src/com/itheima/git/Firstclass.java
@@ -8,8 +8,13 @@ public class Firstclass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    /**
+     * hello project
+     */
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
