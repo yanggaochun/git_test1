07fb084b67fd158743a30b2de211a36bad5a9a2a
yanggaochun <2672381524@qq.com>
2019-06-24 11:25:20 +0800

    修改了本地工程中git内容

--- src/com/itheima/git/Firstclass.java
+++ src/com/itheima/git/Firstclass.java
@@ -8,4 +8,8 @@ public class Firstclass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
